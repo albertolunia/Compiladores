4ddbf42229ad69eb2f7857ec6ce4ec44bc6e448b
Lavínia F. <laviniaaf9@gmail.com>
2024-11-13 19:22:37 -0300

    add files

---
+++ tokens.h
@@ -0,0 +1,15 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#define T_KEYWORD    1
+#define T_IDENTIFIER 2
+#define T_NUMBER     3
+#define T_OPERATOR   4
+#define T_ASSIGN     5
+#define T_PAREN      6
+#define T_SEMICOLON  7
+#define T_STRING     8
+#define T_CHAVES     9
+#define T_OPCOMP     10
+
+#endif
