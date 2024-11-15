b1b7319f6b52e55abfa9d9059b08dac2a4a35b27
albertolunia <albertolunia@gmail.com>
2024-11-15 19:07:56 -0300

    Refatorando o codigo

--- tokens.h
+++ tokens.h
@@ -1,15 +1,25 @@
 #ifndef TOKENS_H
 #define TOKENS_H
 
-#define T_KEYWORD    1
-#define T_IDENTIFIER 2
-#define T_NUMBER     3
-#define T_OPERATOR   4
-#define T_ASSIGN     5
-#define T_PAREN      6
-#define T_SEMICOLON  7
-#define T_STRING     8
-#define T_CHAVES     9
-#define T_OPCOMP     10
+#define T_ASSIGN 1
+#define T_SEMICOLON 2
+#define T_IDENTIFIER 3
+#define T_KEYWORD 4
+#define T_FLOAT 5
+#define T_CHAR 6
+#define T_BOOL 7
+#define T_OPARI 8
+#define T_OPLOG 9
+#define T_OPREL 10
+#define T_CHAVES 11
+#define T_BACKSLASH 12
+#define T_COMMA 13
+#define T_VAR 14
+#define T_SPACE 15
+#define T_STRING 16
+#define T_PARENTESIS 17
+#define T_NEWLINE 18
+#define T_TAB 19
+#define T_COMMENT 20
 
 #endif
